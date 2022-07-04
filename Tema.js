a01dc6f73f116034c238f47842ea04b7e75600e2
AndreeaBulai <107042695+AndreeaBulai@users.noreply.github.com>
2022-07-05 01:20:16 +0300

    Add files via upload
    I applied the knowledge gained so far and created with the help of HTML and JavaScript two buttons to turn on and off a light bulb.

---
+++ Tema.js
@@ -0,0 +1,15 @@
+let myBulb = document.getElementById("bulb");
+
+console.log(myBulb);
+
+function lightOn () {
+	myBulb.src = "./Poza2.jpg" ;
+	console.log("Light is on") ;
+	
+}
+
+function lightOff () {
+	myBulb.src = "./Poza1.jpg" ;
+	console.log = ("Light is off")
+	
+}
